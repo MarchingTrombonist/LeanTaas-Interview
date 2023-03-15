be6b8e57dc44832f04f16989f10c81e470f12ebf
Mac Mai <mmai1@uncc.edu>
2023-03-15 18:37:46 -0400

    Fixed all dates
    Changed all datetimes to ISO8601-like
    
    Renamed chair_start to match other col names

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-import numpy as np
 import pandas as pd
+from datetime import datetime as dt
 
 df = pd.read_csv("LeanTaaS_Infusion_Data_Analyst_Intern_Assignment.csv")
-# print(df)
 
-for colName in df.iloc[:, 3:].columns.values:
-    print(colName + ": ", end="")
-    print(df[colName].unique())
+print(df.head())
+
+print(df.iloc[:, 7:16])
+# rename CHAIR_START to CHAIR_IN to match CHAIR_OUT
+df = df.rename(columns={"CHAIR_START": "CHAIR_IN"})
+
+# Convert all datetimes to %Y-%m-%d %H:%M
+
+# dates
+for col in ["CONTACT_DATE", "APPT_MADE_DATE", "APPT_CANC_DATE"]:
+    df[col] = pd.to_datetime(df[col], infer_datetime_format=True).dt.strftime(
+        "%Y-%m-%d"
+    )
+
+# times
+for col in ["CHAIR_IN", "INFUSION_START", "INFUSION_END", "CHAIR_OUT"]:
+    df[col] = pd.to_datetime(df[col], infer_datetime_format=True).dt.strftime("%H:%M")
+
+# datetimes
+for col in ["APPT_DTTM", "CHECKIN_DTTM", "CHECKOUT_DTTM"]:
+    df[col] = pd.to_datetime(df[col], infer_datetime_format=True).dt.strftime(
+        "%Y-%m-%d %H:%M"
+    )
+
+print(df.iloc[:, 7:16])
