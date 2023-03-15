847b0fe3210c2dd771c02753e27421f578207c65
Mac Mai <mmai1@uncc.edu>
2023-03-15 17:04:53 -0400

    Beginning Analysis
    Identifying first round of issues

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("LeanTaaS_Infusion_Data_Analyst_Intern_Assignment.csv")
+# print(df)
+
+for colName in df.iloc[:, 3:].columns.values:
+    print(colName + ": ", end="")
+    print(df[colName].unique())
