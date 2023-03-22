864873e86cb7d057805be0cbc05352bf26dffac1
Mac Mai <mmai1@uncc.edu>
2023-03-21 21:21:23 -0400

    Removes any with weird times and calculates
    Removes any impossible time jumps and calculates mean waiting time from remaining data.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("LeanTaaS_Infusion_Data_Analyst_Intern_Assignment.csv")
 
@@ -6,6 +7,13 @@ df = pd.read_csv("LeanTaaS_Infusion_Data_Analyst_Intern_Assignment.csv")
 
 # rename CHAIR_START to CHAIR_IN to match CHAIR_OUT
 df = df.rename(columns={"CHAIR_START": "CHAIR_IN"})
+
+
+# Add Notes column; move notes from descriptions
+dfSplit = df["ORDER_DESCRIPTION"].str.split("\n", n=1, expand=True)
+df["ORDER_DESCRIPTION"] = dfSplit[0]
+df.insert(17, "ORDER_NOTES", dfSplit[1])
+
 
 # Convert all datetimes to %Y-%m-%d %H:%M
 # Make all times datetimes using the appt date
@@ -18,22 +26,26 @@ for col in ["CONTACT_DATE", "APPT_MADE_DATE", "APPT_CANC_DATE"]:
 
 # datetimes
 for col in ["APPT_DTTM", "CHECKIN_DTTM", "CHECKOUT_DTTM"]:
-    df[col] = pd.to_datetime(df[col], infer_datetime_format=True).dt.strftime(
-        "%Y-%m-%d %H:%M"
-    )
+    df[col] = pd.to_datetime(df[col], infer_datetime_format=True)
 
 # times
 for col in ["CHAIR_IN", "INFUSION_START", "INFUSION_END", "CHAIR_OUT"]:
-    df[col] = pd.to_datetime(df["APPT_DTTM"]).dt.strftime("%Y-%m-%d ") + df[col]
-    df[col] = pd.to_datetime(df[col], infer_datetime_format=True).dt.strftime(
-        "%Y-%m-%d %H:%M"
-    )
+    df[col] = pd.to_datetime(df["CHECKIN_DTTM"]).dt.strftime("%Y-%m-%d ") + df[col]
+    df[col] = pd.to_datetime(df[col], infer_datetime_format=True)
 
 
-# Add Notes column; move notes from descriptions
-dfSplit = df["ORDER_DESCRIPTION"].str.split("\n", n=1, expand=True)
-df["ORDER_DESCRIPTION"] = dfSplit[0]
-df.insert(17, "ORDER_NOTES", dfSplit[1])
+# Removes rows with dates outside of range
+for col in [
+    "APPT_DTTM",
+    "CHECKIN_DTTM",
+    "CHECKOUT_DTTM",
+    "CHAIR_IN",
+    "INFUSION_START",
+    "INFUSION_END",
+    "CHAIR_OUT",
+]:
+    df = df[~(df[col] > "2021-11-04")]
+    df = df[~(df[col] < "2021-10-31")]
 
 
 # Fix 11/4 extra column (column 20)
@@ -43,24 +55,27 @@ for colNum in range(18, 20):
 
 df = df.iloc[:, ~df.columns.str.match("Unnamed")]
 
+
+# DEPRECATED~~~~~~~~~~~~~~~~~~~~~~
 # Check that all appointments follow the correct order
 # CHECKIN_DTTM < CHAIR_IN < INFUSION_START < INFUSION_END < CHAIR_OUT < CHECKOUT_DTTM
 # Col Indices: 8, 12, 13, 14, 15, 9
 # ["CHECKIN_DTTM","CHAIR_IN","INFUSION_START","INFUSION_END","CHECKOUT_DTTM"]
-# Split by appt status
-
 # Returns all rows where all times exist and are in an increasing order
 # Both removes rows with nans and rows where times are out of order
 # Can probably be optimized, but I couldn't figure out a way to use pandas parsing
 # .is_monotonic_increasing is an attribute, not a function, so apply() doesn't work
-df_ordered = df.iloc[
-    [
-        row
-        for row in df.index
-        if (df.iloc[row, [8, 12, 13, 14, 9]].is_monotonic_increasing)
-    ],
-    :,
-]
+
+# df_ordered = df.iloc[
+#     [
+#         row
+#         for row in df.index
+#         if (df.iloc[row, [8, 12, 13, 14, 9]].is_monotonic_increasing)
+#     ],
+#     :,
+# ]
+
+# Split by appt status
 
 # # Arrived: 1 appt
 # print(
@@ -86,29 +101,54 @@ df_ordered = df.iloc[
 #         :, [5, 8, 12, 13, 14, 9]
 #     ]
 # )
+# DEPRECATED~~~~~~~~~~~~~~~~~~~~~~
 
-# print(df.columns.values)
-# df.to_csv("Infusion_Fixed.csv")
-
-dfGrouped = df
-for col in [
-    "CHECKIN_DTTM",
-    "CHAIR_IN",
-    "INFUSION_START",
-    "INFUSION_END",
-    "CHECKOUT_DTTM",
-]:
-    dfGrouped[col] = dfGrouped.groupby("INPATIENT_DATA_ID_x")[col].max()
-
-print(
-    dfGrouped[
-        [
-            "INPATIENT_DATA_ID_x",
-            "CHECKIN_DTTM",
-            "CHAIR_IN",
-            "INFUSION_START",
-            "INFUSION_END",
-            "CHECKOUT_DTTM",
-        ]
+dfAppt = df.drop_duplicates("INPATIENT_DATA_ID_x")[
+    [
+        "INPATIENT_DATA_ID_x",
+        "APPT_LENGTH",
+        "APPT_STATUS_NAME",
+        "APPT_DTTM",
+        "CHECKIN_DTTM",
+        "CHAIR_IN",
+        "INFUSION_START",
+        "INFUSION_END",
+        "CHAIR_OUT",
+        "CHECKOUT_DTTM",
+        "ORDER_STATUS",
     ]
+]
+
+for appt in dfAppt["INPATIENT_DATA_ID_x"]:
+    for colName in ["CHECKIN_DTTM", "CHAIR_IN", "INFUSION_START"]:
+        dfAppt.loc[dfAppt["INPATIENT_DATA_ID_x"] == appt, colName] = (
+            df.loc[df["INPATIENT_DATA_ID_x"] == appt, colName].dropna().min()
+        )
+    for colName in ["CHECKOUT_DTTM", "CHAIR_OUT", "INFUSION_END"]:
+        dfAppt.loc[dfAppt["INPATIENT_DATA_ID_x"] == appt, colName] = (
+            df.loc[df["INPATIENT_DATA_ID_x"] == appt, colName].dropna().max()
+        )
+
+        # print(
+        #     dfAppt[
+        # [
+        #     "INPATIENT_DATA_ID_x",
+        #     "CHECKIN_DTTM",
+        #     "CHAIR_IN",
+        #     "INFUSION_START",
+        #     "INFUSION_END",
+        #     "CHECKOUT_DTTM",
+        # ]
+#     ]
+# )
+
+# Remove any with infusions that end before they've checked in
+df_ordered = dfAppt[dfAppt["CHECKIN_DTTM"] < dfAppt["INFUSION_END"]]
+
+df_ordered["APPT_DELTA"] = df_ordered["INFUSION_END"] - df_ordered["CHECKIN_DTTM"]
+print(
+    df_ordered[["INPATIENT_DATA_ID_x", "INFUSION_END", "CHECKIN_DTTM", "APPT_DELTA"]]
+    .dropna()
+    .sort_values("APPT_DELTA")
 )
+print(df_ordered["APPT_DELTA"].dropna().mean())
